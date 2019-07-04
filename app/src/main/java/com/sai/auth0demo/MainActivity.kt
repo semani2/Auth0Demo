ce044e219a3b855be8da20324fa28ffa5b5ee700
Sai Emani <Sai.Emani@docusign.com>
2019-07-04 13:06:43 -0700

    Basic user login implemented.

--- app/src/main/java/com/sai/auth0demo/MainActivity.kt
+++ app/src/main/java/com/sai/auth0demo/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.sai.auth0demo
 
+import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.provider.AuthCallback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var account: Auth0
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +42,44 @@ class MainActivity : AppCompatActivity() {
      * This function performs authentication using the Web Auth Provider from Auth0
      */
     private fun performWebLogin() {
+        if (::account.isInitialized) {
+            WebAuthProvider.login(account)
+                .withScheme("demo")
+                .withAudience(String.format("https://%s/userinfo", getString(R.string.com_auth0_domain)))
+                .start(this, object: AuthCallback {
+                    override fun onSuccess(credentials: Credentials) {
+                        runOnUiThread {
+                            current_auth_status_text_view.setText(R.string.logged_in)
+                            token_text_view.text = credentials.accessToken
 
+                            Log.d(TAG, "Scope ${credentials.scope}")
+                            Log.d(TAG, "Type ${credentials.type}")
+                            Log.d(TAG, "Expires in ${credentials.expiresIn}")
+                        }
+                    }
+
+                    override fun onFailure(dialog: Dialog) {
+                        runOnUiThread {
+                            dialog.show()
+                        }
+                    }
+
+                    override fun onFailure(exception: AuthenticationException?) {
+                        runOnUiThread {
+                            current_auth_status_text_view.setText(R.string.login_failed)
+                            token_info_text_view.setText(R.string.failure_reason)
+                            token_text_view.text = exception?.message
+
+                            Log.e(TAG, "Login failed", exception)
+                        }
+                    }
+
+                })
+        }
     }
 
     private fun setupAuth0() {
-        val account = Auth0(this)
+        account = Auth0(this)
         account.isOIDCConformant = true
     }
 }
